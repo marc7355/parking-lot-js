6af03e4a822b76857c529f7d81e0662455fb4bf6
marc7355 <marc.lemming03@gmail.com@gmail.com>
2019-08-19 13:03:44 +0200

    OPG 1

--- js/ParkingLot.js
+++ js/ParkingLot.js
@@ -13,16 +13,19 @@ class ParkingLot {
         if (this.checkedInCars[licensePlate] != undefined) {
             throw new Error(`${licensePlate} holder allerede på pladsen!`);
         } else {
-            this.checkedInCars[licensePlate] = "CHECKED IN";
+            this.checkedInCars[licensePlate] = new Date();
             openGate(this.entranceGate);
         }
     }
 
     checkout(licensePlate) {
-        if (this.checkedInCars[licensePlate] != 'CHECKED IN') {
+        const checkinTime = this.checkedInCars[licensePlate]
+        console.log(checkinTime)
+        if (checkinTime == undefined || checkinTime.constructor != Date) {
             throw new Error(`${licensePlate} holder ikke på pladsen!`);
         } else {
-            this.checkedInCars[licensePlate] = 40;
+            const time = Math.round ((new Date () - checkinTime) / 1000);
+            this.checkedInCars[licensePlate] = 15 * (Math.floor(time / 15) + 1);
             return this.checkedInCars[licensePlate];
         }
     }
